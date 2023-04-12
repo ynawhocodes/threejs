f9a04fdeb869e862e6f8ad1ab8d564c6c9c1cce5
yanwhocodes <ynawhocodes@gmail.com>
2023-04-12 15:47:49 +0900

    feat: add renderer properties

--- src/index.js
+++ src/index.js
@@ -9,7 +9,10 @@ if (WEBGL.isWebGLAvailable()) {
     const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
     camera.position.z = 2;
     // renderer
-    const renderer = new THREE.WebGLRenderer();
+    const renderer = new THREE.WebGLRenderer({
+        alpha: true,
+        antialias: true
+    });
     renderer.setSize(window.innerWidth, window.innerHeight);
     document.body.appendChild(renderer.domElement);
     // mesh 01
